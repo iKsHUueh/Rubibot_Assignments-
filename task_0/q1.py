44d6e59293a86113f47b0275f3cdf4c9d2538455
iKsHUueh <ikshu874@gmail.com>
2026-05-27 23:41:55 +0530

    Add files via upload

--- task_0/q1.py
+++ task_0/q1.py
@@ -1,1 +1,17 @@
+face = ["red", "blue", "Red", "white", "RED", "blue", "White", "green", "Green"]
+counts = {"R": 0, "B": 0, "W": 0, "G": 0}
 
+for color in face:
+    upper_color = color.upper()
+
+    if upper_color == "RED":
+        counts["R"] += 1
+    elif upper_color == "BLUE":
+        counts["B"] += 1
+    elif upper_color == "WHITE":
+        counts["W"] += 1
+    elif upper_color == "GREEN":
+        counts["G"] += 1
+
+for initial, count in counts.items():
+    print(f"{initial} appears {count} times")
